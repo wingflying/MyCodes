eb6088d60ca44376cafdda92810cd0fe4e09f47e
wingflying <wingflying@139.com>
2021-02-19 15:10:37 +0800

    Initial commit

---
+++ Python/aliensEx/settings.py
@@ -0,0 +1,12 @@
+"""
+存储外星人入侵的所有设置类
+"""
+
+class Settings():
+
+    def __init__(self):
+        #初始化游戏设置
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
---
+++ c/mainAdd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 3;
+	int b;
+	
+	a = a + 2;
+	b = a + 2;
+
+	printf("The value is %d : %d\n", a, b);
+		return 0;
+}
