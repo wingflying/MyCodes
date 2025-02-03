834613a91684586d36f98d2734db646d39677528
wingflying <wingflying@139.com>
2025-02-03 14:38:53 +0800

    配置更新

--- c/basicEX/src/main.cpp
+++ c/basicEX/src/main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include <stdio.h>
 
-int main (int argc, char *argv[])
+int main(void)
 {
-	std::cout << "Hello CMake!" << std::endl;
-	return 0;
-}
- 
+	int year;
+
+	printf("Please input the value of year:\n");
+	scanf("%d", &year);
+
+	if((!(int)year) == year && year <= 0)
+		printf("The input value of year is valid!\n");
+	else
+	{
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year %400 == 0))
+			printf("The input value of %d is a leap year!\n", year);
+		else
+			printf("The input value of %d is not a leap year!\n", year);
+	}
+}  
